ce8c73c1581cd9b84f5cc9de8215cbe261542838
Chris Sanchez <chrisvsanchez@gmail.com>
2020-05-21 23:52:18 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,61 +1,19 @@
-// const codes = [
-//   "ArrowUp",
-//   "ArrowUp",
-//   "ArrowDown",
-//   "ArrowDown",
-//   "ArrowLeft",
-//   "ArrowRight",
-//   "ArrowLeft",
-//   "ArrowRight",
-//   "b",
-//   "a"
-// ];
-// 
-// function init() {
-//   let index = 0;
-//  document.body.addEventListener("keydown", onKeyDownHandler(e){
-// function onKeyDownHandler(e) {
-//   const key = e.key;
-//  
-//   if (key === code[index]) {
-//     index++;
-//  
-//     if (index === code.length) {
-//       alert("Hurray!");
-//  
-//       index = 0;
-//     }
-//   } else {
-//     index = 0;
-//    }
-//   }
-// })
-// }
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
 
-
-
-	function init() {
-  // your code here	  //
-  var index = 0
-  document.body.addEventListener('keydown', onKeyDownHandler());
-
-  function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which)
-    console.log('key', key)
-    if (key === code[index]) {
-      index++
-
-      if (index === code.length) {
-        alert("YOU DID IT!")
-        index = 0
-      }
-    } else {
-      index = 0
-    }
+function init() 
+index = 0;
+  document.body.addEventListener("keydown", (event) => {
+    // Now, how can we check for which specific key was pressed?
   }
-}	
-
-
-
-init();
+}
