c9c14d422f6e54e2dd1ce654d21a4c9fcf9c18b1
Chris Sanchez <chrisvsanchez@gmail.com>
2020-05-21 23:51:37 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,34 +1,61 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+// const codes = [
+//   "ArrowUp",
+//   "ArrowUp",
+//   "ArrowDown",
+//   "ArrowDown",
+//   "ArrowLeft",
+//   "ArrowRight",
+//   "ArrowLeft",
+//   "ArrowRight",
+//   "b",
+//   "a"
+// ];
+// 
+// function init() {
+//   let index = 0;
+//  document.body.addEventListener("keydown", onKeyDownHandler(e){
+// function onKeyDownHandler(e) {
+//   const key = e.key;
+//  
+//   if (key === code[index]) {
+//     index++;
+//  
+//     if (index === code.length) {
+//       alert("Hurray!");
+//  
+//       index = 0;
+//     }
+//   } else {
+//     index = 0;
+//    }
+//   }
+// })
+// }
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
 
-  document.body.addEventListener('keydown', onKeyDownHandler);
-  let index = 0;
+
+	function init() {
+  // your code here	  //
+  var index = 0
+  document.body.addEventListener('keydown', onKeyDownHandler());
+
   function onKeyDownHandler(e) {
-
     const key = parseInt(e.detail || e.which)
+    console.log('key', key)
     if (key === code[index]) {
       index++
 
       if (index === code.length) {
-        return alert("Hurray!")
+        alert("YOU DID IT!")
         index = 0
       }
     } else {
       index = 0
-      }
     }
-  }	
+  }
+}	
 
 
+
+init();
